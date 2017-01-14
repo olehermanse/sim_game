9b904ddd26f0d17fba14adc70983947e0b9c74c0
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2017-01-14 09:56:52 +0100

    More detailed instructions

--- travis/install.sh
+++ travis/install.sh
@@ -1,14 +1,14 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    # Mac OS X
     brew update
     brew install python3
     brew tap Homebrew/python
     pip3 install -U pip wheel
-    #pip3 install -r requirements.txt
-    #pip3 install --only-binary pygame pygame
     brew install sdl sdl_image sdl_mixer sdl_ttf smpeg portmidi
     pip3 install hg+http://bitbucket.org/pygame/pygame
     pip3 install pytest
 else
+    # Linux
     pip3 install -U pip wheel
     pip3 install -r requirements.txt
 fi
