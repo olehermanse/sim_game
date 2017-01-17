27c8b12e731b2cc6003b9718bd7c965f3bcd76ab
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2017-01-17 23:38:31 +0100

    Added Color and Renderer classes

--- src/game.py
+++ src/game.py
@@ -6,7 +6,7 @@ __authors__    = ["Ole Herman Schumacher Elgesem"]
 __license__    = "MIT"
 # This file is subject to the terms and conditions defined in 'LICENSE'
 
-from graphics import SpriteObject, TextObject, Rectangle
+from graphics import SpriteObject, TextObject, Rectangle, Renderer
 
 class Game:
     def __init__(self, window):
@@ -15,9 +15,9 @@ class Game:
         #self.message = TextObject("Hello, world", size=32, pos=(400,300), vel=(0,100), acc=(0,-100))
         self.rectangle = Rectangle(100,20, stroke=(255,255,255,255), pos=(400,300), vel=(0,100), acc=(0,-100))
 
-
+    # TODO: change to batch drawing for performance
     def draw(self):
-        self.window.clear()
+        Renderer.start(self.window)
 
         #self.cat.draw()
         #self.message.draw()
